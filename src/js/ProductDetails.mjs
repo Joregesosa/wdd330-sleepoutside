596b6176e035b981e76d15c196cf3b8c5ac6e569
joregesosa <joregesosa@gmail.com>
2026-03-17 23:31:04 -0400

    Add category icons and update product listing structure
    - Added SVG icons for categories: backpacks, hammocks, and sleeping bags.
    - Updated the category list in index.html to include links to product listings for tents, backpacks, sleeping bags, and hammocks.
    - Enhanced CSS with styles for category list and product category layout.
    - Modified ProductData and ProductList classes to fetch data based on category.
    - Created product-listing.js to initialize product list based on selected category.
    - Updated utils.mjs to generalize parameter retrieval from URL.
    - Created a new product listing HTML file with appropriate structure and linked scripts.

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,40 +1,41 @@
 import { qs, setLocalStorage } from "./utils.mjs";
 
 export default class ProductDetails {
+  constructor(product_id, dataSource) {
+    this.product_id = product_id;
+    this.dataSource = dataSource;
+    this.product = {};
+  }
+  async init() {
+    this.product = await this.dataSource.findProductById(this.product_id);
+    this.renderProductDetails();
+    qs("#addToCart").addEventListener(
+      "click",
+      this.addProductToCart.bind(this),
+    );
+  }
 
-    constructor(product_id, dataSource) {
-        this.product_id = product_id;
-        this.dataSource = dataSource;
-        this.product = {};
-    }
+  addProductToCart() {
+    const product = this.product;
+    const cart = JSON.parse(localStorage.getItem("so-cart")) || [];
+    setLocalStorage("so-cart", [...cart, product]);
+  }
 
-    async init() {
-        this.product = await this.dataSource.findProductById(this.product_id);
-        this.renderProductDetails();
-        qs('#addToCart')
-            .addEventListener('click', this.addProductToCart.bind(this));
-    }
-
-    addProductToCart() {
-        const product = this.product;
-        const cart = JSON.parse(localStorage.getItem("so-cart")) || [];
-        setLocalStorage("so-cart", [...cart, product]);
-    }
-
-    renderProductDetails() {
-
-        const template = `
+  renderProductDetails() {
+    const template = /* html */ `
             <h3>${this.product.Brand.Name}</h3>
 
             <h2 class="divider">${this.product.Name}</h2>
 
             <img class="divider"
-                src="${this.product.Image}"
-                alt="${this.product.Name}" />
+                src="${this.product.Images.PrimaryLarge}"
+                alt="${this.product.Name}" 
+                loading="lazy"
+            />
 
             <p class="product-card__price">$${this.product.FinalPrice}</p>
 
-            <p class="product__color">${this.product.Colors.map(color => color.ColorName).join(", ")}</p>
+            <p class="product__color">${this.product.Colors.map((color) => color.ColorName).join(", ")}</p>
 
             <p class="product__description">
                 ${this.product.DescriptionHtmlSimple}
@@ -44,6 +45,6 @@ export default class ProductDetails {
                 <button id="addToCart" data-id="${this.product.ID}">Add to Cart</button>
             </div>
       `;
-        qs('.product-detail').innerHTML = template;
-    }
+    qs(".product-detail").innerHTML = template;
+  }
 }
